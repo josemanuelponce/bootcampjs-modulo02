16d848ca41d5c4954cc26c731290fb1bc6630437
Nombre prueba <prueba@hotmail.com>
2023-07-21 13:04:56 +0200

    Laboratorio finalizado

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
-import "./style.css";
+console.log("Cena de cumpleaños entre amigos");
 
-console.log("Hello Typescript!");
+console.log("---------------------");
+
+console.log("6 amigos");
+console.log("120 euros de ticket de cena");
+console.log("18 euros valor de bebidas");
+
+console.log("Resultado = " + (120 - 18) / 6);
